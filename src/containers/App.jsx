ecb4a40593972c6e31cdb3d5ca9dad3a5faf93ff
Dean McNeil <deanolmcneil@gmail.com>
2023-04-18 20:25:34 -0400

    Add basic reducer structure

---
+++ src/containers/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PokemonSelector from "../components/PokemonSelector";
+
+const App = () => {
+  return (
+    <>
+      <PokemonSelector />
+    </>
+  );
+};
+
+export default App;
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,45 @@
 import { combineReducers } from "redux";
 import * as constants from "./constants";
+// ************************************************************ initial states ************************************************************
+const initialStateCurrentPokemon = {
+  currentPokemonName: "",
+  currentPokemonPictureUrl: "",
+  currentPokemonEggGroups: [],
+};
 
-export const rootReducer = combineReducers({});
+const initialStatePokemonSelector = {
+  completePokemonObjectArray: [],
+};
+
+const initialStateSearchfield = {
+  searchfield: "",
+};
+// ************************************************************ reducers ************************************************************
+const currentPokemon = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case constants.SELECT_POKEMON:
+      return { ...state, currentPokemonName: action.payload };
+    default:
+      return state;
+  }
+};
+
+const pokemonSelector = (state = initialStatePokemonSelector, action = {}) => {
+  switch (action.type) {
+    case constants.POPULATE_POKEMON_SELECTOR:
+      return { ...state, completePokemonObjectArray: action.payload };
+    default:
+      return state;
+  }
+};
+
+const searchfield = (state = initialStateSearchfield, action = {}) => {
+  switch (action.type) {
+    case constants.UPDATE_SEARCHFIELD:
+      return { ...state, searchfield: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const rootReducer = combineReducers({ current });
