e373c200cc063023fb4cec9f4cefed9b081a2b62
Dean McNeil <deanolmcneil@gmail.com>
2023-04-21 15:05:57 -0400

    Remove some old files

--- src/old-files/App.js
+++ src/old-files/App.js
@@ -2,24 +2,11 @@ class App extends Component {
   render() {
     return (
       <div className="container-fluid text-center">
-        <div className="row">
-          <LogoWrapperSmallScreen
-            currentlySelectedPokemonName={currentlySelectedPokemonName}
-          />
-          <div className="col-12 col-lg-5 d-flex justify-content-center">
-            <div className="row align-items-center">
-              <PokemonSelector />
-            </div>
-          </div>
-          <LogoWrapperLargeScreen
-            currentlySelectedPokemonName={currentlySelectedPokemonName}
-          />
-          <EggGroupWrapper
-            eggGroups={eggGroups}
-            currentlySelectedPokemonName={currentlySelectedPokemonName}
-            cleanPokemonName={this.cleanPokemonName}
-          />
-        </div>
+        <EggGroupWrapper
+          eggGroups={eggGroups}
+          currentlySelectedPokemonName={currentlySelectedPokemonName}
+          cleanPokemonName={this.cleanPokemonName}
+        />
       </div>
     );
   }
